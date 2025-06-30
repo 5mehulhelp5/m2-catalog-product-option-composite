9c2a63741e32b6012f2792640719f7ea0ae77a10
Andreas Knollmann <a.knollmann@seak.dev>
2025-06-30 13:37:39 +0200

    moved generating of bundle option product options to block

---
+++ src/Block/Catalog/Product/View/Type/Bundle/Options/Options.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrangible\CatalogProductOptionComposite\Block\Catalog\Product\View\Type\Bundle\Options;
+
+use Infrangible\CatalogProductOptionComposite\Helper\Output;
+use Magento\Bundle\Model\Option;
+use Magento\Framework\View\Element\Template;
+
+/**
+ * @author      Andreas Knollmann
+ * @copyright   2014-2025 Softwareentwicklung Andreas Knollmann
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT
+ */
+class Options extends Template
+{
+    /** @var Output */
+    protected $outputHelper;
+
+    /** @var Option */
+    private $bundleOption;
+
+    public function __construct(Template\Context $context, Output $outputHelper, array $data = [])
+    {
+        parent::__construct(
+            $context,
+            $data
+        );
+
+        $this->outputHelper = $outputHelper;
+    }
+
+    public function getBundleOption(): Option
+    {
+        return $this->bundleOption;
+    }
+
+    public function setBundleOption(Option $bundleOption): void
+    {
+        $this->bundleOption = $bundleOption;
+    }
+
+    public function renderBundleOptionsHtml(): string
+    {
+        return $this->outputHelper->renderBundleOptionsHtml(
+            $this,
+            $this->getBundleOption()
+        );
+    }
+}
--- src/Plugin/Bundle/Block/Catalog/Product/View/Type/Bundle.php
+++ src/Plugin/Bundle/Block/Catalog/Product/View/Type/Bundle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Infrangible\CatalogProductOptionComposite\Plugin\Bundle\Block\Catalog\Product\View\Type;
 
-use Infrangible\CatalogProductOptionComposite\Helper\Output;
+use Infrangible\CatalogProductOptionComposite\Block\Catalog\Product\View\Type\Bundle\Options\Options;
 use Magento\Bundle\Model\Option;
 
 /**
@@ -14,24 +14,20 @@ use Magento\Bundle\Model\Option;
  */
 class Bundle
 {
-    /** @var Output */
-    protected $outputHelper;
-
-    public function __construct(Output $outputHelper)
-    {
-        $this->outputHelper = $outputHelper;
-    }
-
     public function afterGetOptionHtml(
         \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle $subject,
         string $result,
         Option $bundleOption
     ): string {
-        $optionsHtml = $this->outputHelper->renderBundleOptionsHtml(
-            $subject,
-            $bundleOption
-        );
+        /** @var Options $childBlock */
+        $childBlock = $subject->getChildBlock('product.info.bundle.options.options');
 
-        return $result . $optionsHtml;
+        if ($childBlock) {
+            $childBlock->setBundleOption($bundleOption);
+
+            return $result . $childBlock->toHtml();
+        } else {
+            return $result;
+        }
     }
 }
