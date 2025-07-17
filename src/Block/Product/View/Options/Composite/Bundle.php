6cb728956b511b51c4bd22aacefd68ecda5dfbc2
Andreas Knollmann <a.knollmann@seak.dev>
2025-07-17 20:14:00 +0200

    update to use helper

--- src/Block/Product/View/Options/Composite/Bundle.php
+++ src/Block/Product/View/Options/Composite/Bundle.php
@@ -10,10 +10,8 @@ use Infrangible\CatalogProductOptionComposite\Block\Product\View\Options\Composi
 use Infrangible\CatalogProductOptionComposite\Helper\Data;
 use Infrangible\Core\Helper\Registry;
 use Magento\Bundle\Model\Option;
-use Magento\Catalog\Model\Product\Option\Value;
 use Magento\Catalog\Pricing\Price\CustomOptionPrice;
 use Magento\Catalog\Pricing\Price\CustomOptionPriceInterface;
-use Magento\Framework\DataObject;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Locale\Format;
 use Magento\Framework\Pricing\Render;
@@ -32,11 +30,8 @@ class Bundle extends Composite
     /** @var Format */
     protected $localeFormat;
 
-    /** @var \Magento\Framework\Pricing\Helper\Data */
-    protected $pricingHelper;
-
-    /** @var \Magento\Catalog\Helper\Data */
-    protected $catalogHelper;
+    /** @var \Infrangible\CatalogProductOption\Helper\Data */
+    protected $productOptionHelper;
 
     /** @var Option */
     private $bundleOption;
@@ -48,8 +43,7 @@ class Bundle extends Composite
         Variables $variables,
         Data $helper,
         Format $localeFormat,
-        \Magento\Framework\Pricing\Helper\Data $pricingHelper,
-        \Magento\Catalog\Helper\Data $catalogHelper,
+        \Infrangible\CatalogProductOption\Helper\Data $productOptionHelper,
         array $data = []
     ) {
         parent::__construct(
@@ -62,8 +56,7 @@ class Bundle extends Composite
 
         $this->helper = $helper;
         $this->localeFormat = $localeFormat;
-        $this->pricingHelper = $pricingHelper;
-        $this->catalogHelper = $catalogHelper;
+        $this->productOptionHelper = $productOptionHelper;
     }
 
     public function getBundleOption(): Option
@@ -155,97 +148,6 @@ class Bundle extends Composite
 
     public function getOptionsJsonConfig(): string
     {
-        $config = [];
-
-        /** @var \Magento\Catalog\Model\Product\Option $option */
-        foreach ($this->getOptions() as $option) {
-            if ($option->hasValues()) {
-                $tmpPriceValues = [];
-
-                /** @var Value $value */
-                foreach ($option->getValues() as $valueId => $value) {
-                    $tmpPriceValues[ $valueId ] = $this->getPriceConfiguration($value);
-                }
-
-                $priceValue = $tmpPriceValues;
-            } else {
-                $priceValue = $this->getPriceConfiguration($option);
-            }
-
-            $config[ $option->getId() ] = $priceValue;
-        }
-
-        $configObj = new DataObject(
-            [
-                'config' => $config,
-            ]
-        );
-
-        //pass the return array encapsulated in an object for the other modules to be able to alter it eg: weee
-        $this->_eventManager->dispatch(
-            'catalog_product_option_price_configuration_after',
-            ['configObj' => $configObj]
-        );
-
-        $config = $configObj->getData('config');
-
-        return $this->json->encode($config);
-    }
-
-    /**
-     * @param Value|\Magento\Catalog\Model\Product\Option $option
-     */
-    protected function getPriceConfiguration($option): array
-    {
-        $optionPrice = $option->getPrice(true);
-
-        if ($option->getPriceType() !== Value::TYPE_PERCENT) {
-            $optionPrice = $this->pricingHelper->currency(
-                $optionPrice,
-                false,
-                false
-            );
-        }
-
-        return [
-            'prices' => [
-                'oldPrice'   => [
-                    'amount'      => $this->pricingHelper->currency(
-                        $option->getRegularPrice(),
-                        false,
-                        false
-                    ),
-                    'adjustments' => [],
-                ],
-                'basePrice'  => [
-                    'amount' => $this->catalogHelper->getTaxPrice(
-                        $option->getProduct(),
-                        $optionPrice,
-                        false,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        false
-                    ),
-                ],
-                'finalPrice' => [
-                    'amount' => $this->catalogHelper->getTaxPrice(
-                        $option->getProduct(),
-                        $optionPrice,
-                        true,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        false
-                    ),
-                ]
-            ],
-            'type'   => $option->getPriceType(),
-            'name'   => $option->getTitle(),
-        ];
+        return $this->productOptionHelper->getOptionsJsonConfig($this->getOptions());
     }
 }
